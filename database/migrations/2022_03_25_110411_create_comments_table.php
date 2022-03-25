694e5b0902607b3e475634b1185109c233d6f690
NetrebaIgor <71346047+NetrebaIgor@users.noreply.github.com>
2022-03-25 15:50:42 +0200

    hotfix migration

--- database/migrations/2022_03_25_110411_create_comments_table.php
+++ database/migrations/2022_03_25_110411_create_comments_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained()->onDelete('cascade')->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate('cascade')->nullable();
             $table->string('email');
             $table->string('name');
             $table->text('text');
